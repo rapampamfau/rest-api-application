4bb8740d3e7f2b49a940925d5ef2dfab9395b936
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-05-07 16:41:03 +0200

    Add heroku configuration

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,20 +1,19 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
-
+//public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	@Override
-	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}
+//	@Override
+//	public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(TasksApplication.class);
+//	}
 }
