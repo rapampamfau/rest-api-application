92e710807a6277c809d1510885ed2242be91a1f0
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-05-21 19:04:31 +0200

    23.3 CC implementation

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Mail {
+
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final String toCc;
+}
