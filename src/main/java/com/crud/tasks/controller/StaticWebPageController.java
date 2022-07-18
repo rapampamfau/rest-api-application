7df7173c227dd5d44c33cadfe8fe50b2435ba520
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-07-18 20:18:52 +0200

    Url change in StaticWebPageController

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 @Controller
 public class StaticWebPageController {
 
-    @RequestMapping("/")
+    @RequestMapping("http://localhost:8080/exercise")
     public String index(Map<String, Object> model) {
         model.put("variable", "My Thymeleaf variable");
         model.put("one", 1);
