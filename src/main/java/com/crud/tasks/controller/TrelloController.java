96d1f79efba4ef8979c2d395e6c2f772bf83bb71
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-07-20 14:31:11 +0200

    Added @Autowired annotation

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -5,6 +5,7 @@ import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.facade.TrelloFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TrelloController {
 
+    @Autowired
     private final TrelloFacade trelloFacade;
 
     @GetMapping("boards")
