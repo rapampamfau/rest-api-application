48b1fff87ab7ff41e872044ab944b68a8ae6f95e
Rafal Szczepanczyk <rafal.szczepanczyk7@gmail.com>
2022-07-23 14:57:24 +0200

    32.2 email template Thymeleaf

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -23,6 +27,10 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "https://rapampamfau.github.io/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_msg", "Have a great day :)");
+        context.setVariable("companyName", companyConfig.getCompanyName());
+        context.setVariable("companyEmail", companyConfig.getCompanyEmail());
+        context.setVariable("companyPhone", companyConfig.getCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
